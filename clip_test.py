77b14fc49ac7e1d8eb8fd71c59098891f8c3a85f
Amir <amir.gheser@studenti.unitn.it>
2024-04-21 18:15:15 +0200

    Imagenet-v2 loader

--- clip_test.py
+++ clip_test.py
@@ -10,10 +10,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def process_batch(loader: torch.utils.data.DataLoader,
+                    classes: list,
+                    id2class: dict,
+                    device: str = "cpu"):
+    dataiter = iter(loader)
+    images, labels = next(dataiter)
+    images.to(device)
+
+    text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in classes]).to(device)
+
+    with torch.no_grad():
+        image_features = model.encode_image(images.to(device))
+        text_features = model.encode_text(text_inputs)
+
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+        similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+    for i in range(similarity.shape[0]):
+        values, indices = similarity[i].topk(5)
+
+        print("\nTop predictions:\n")
+        for value, index in zip(values, indices):
+            print(f"{id2class[index.item()]:>16s}: {100 * value.item():.2f}%")
+        print("\nTrue label:{}".format(id2class[labels[i].item()]))
+
+        show_image(images[i], f"{id2class[labels[i].item()]} - {labels[i]}")
+
 def show_image(image, label):
     image = image.numpy()
     plt.title(f"Image of {label}")
-    plt.imshow(np.transpose(image.astype(np.uint8), (1, 2, 0)))
+    img = np.transpose((image * 255).astype(np.uint8), (1, 2, 0))
+    plt.imshow(img)
     plt.show()
 
 all_wnids = py_vars.all_wnids
@@ -29,46 +58,28 @@ else:
 
 # mean = [0.485, 0.456, 0.406]
 # std = [0.229, 0.224, 0.225]
-# transform = transforms.Compose([transforms.Resize(256),
-#                                 transforms.CenterCrop(224),
-#                                 transforms.ToTensor(),
-#                                 transforms.Normalize(mean, std)])
+no_transform = transforms.Compose([transforms.Resize(256),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor()])
 
 model, preprocess = clip.load('ViT-B/32', device)
 
 imagenet_a_wnids = os.listdir('./data/imagenet-a')
 imagenet_a_wnids.remove('README.txt')
-assert sorted(imagenet_a_wnids) == sorted(py_vars.imagenet_a_wnids)
+assert len(imagenet_a_wnids) == 200
 
-imagenet_a_mask = [wnid in set(imagenet_a_wnids) for wnid in all_wnids]
-
+print("Processing ImageNet-A")
 imagenet_A = datasets.ImageFolder(root='./data/imagenet-a', transform=preprocess)
 imagenet_A_loader = torch.utils.data.DataLoader(imagenet_A, batch_size=10, shuffle=True)
 
-dataiter = iter(imagenet_A_loader)
+id2class = {imagenet_A.class_to_idx[c] : py_vars.num2class[c] for c in imagenet_A.classes}
 
+process_batch(imagenet_A_loader, id2class.items(), id2class, device)
 
-image, labels = next(dataiter)
+print("Processing ImageNet-V2")
+imagenet_v2 = datasets.ImageFolder(root='./data/imagenetv2-matched-frequency-format-val', transform=no_transform)
+imagenet_v2_loader = torch.utils.data.DataLoader(imagenet_v2, batch_size=10, shuffle=True)
 
-image.to(device)
+id2class = {imagenet_v2.class_to_idx[c] : py_vars.num2class_v2[int(c)] for c in imagenet_v2.classes}
 
-text_inputs = torch.cat([clip.tokenize(f"a photo of a {py_vars.num2class[c]}") for c in imagenet_A.classes]).to(device)
-
-with torch.no_grad():
-    # image_features = model.encode_image(image)
-    image_features = model.encode_image(image.to(device))
-    text_features = model.encode_text(text_inputs)
-
-image_features /= image_features.norm(dim=-1, keepdim=True)
-text_features /= text_features.norm(dim=-1, keepdim=True)
-similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-
-for i in range(similarity.shape[0]):
-    values, indices = similarity[i].topk(5)
-
-    print("\nTop predictions:\n")
-    for value, index in zip(values, indices):
-        print(f"{py_vars.num2class[imagenet_A.classes[index]]:>16s}: {100 * value.item():.2f}%")
-    print("\nTrue label:{}".format(py_vars.num2class[imagenet_A.classes[labels[i]]]))
-
-    show_image(image[i], py_vars.num2class[imagenet_A.classes[labels[i]]])
+process_batch(imagenet_v2_loader, py_vars.num2class_v2.items(), id2class, device)
