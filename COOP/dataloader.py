e70b3df5b4c531abcec7ba985c9f5a7b5e239afe
matteomaske <matteomascherin@gmail.com>
2024-05-05 16:48:34 +0200

    Adapting code for TPT

--- COOP/dataloader.py
+++ COOP/dataloader.py
@@ -2,6 +2,7 @@ from torch.utils import data
 from torch.utils.data import random_split
 from torchvision import datasets
 import os
+import py_vars
 
 def get_data(dataset_name, batch_size, transform, shuffle=True, train_size=0.8, val_size=0.1):
     """
@@ -18,15 +19,19 @@ def get_data(dataset_name, batch_size, transform, shuffle=True, train_size=0.8, 
     if dataset_name == "cifar10":
         download = not (os.path.exists(os.path.join("data/cifar-10-python")))
         dataset = datasets.CIFAR10(root="./data", download=download, transform=transform)
+        id2class = {dataset.class_to_idx[c] : c for c in dataset.classes}
     elif dataset_name == "cifar100":
         download = not (os.path.exists(os.path.join("data/cifar-100-python")))
         dataset = datasets.CIFAR100(root="./data", download=download, transform=transform)
+        id2class = {dataset.class_to_idx[c] : c for c in dataset.classes}
     elif dataset_name == "imagenet_v2":
         dataset = datasets.ImageFolder(root="./data/imagenetv2-matched-frequency-format-val", transform=transform)
-        dataset.classes = sorted(dataset.classes, key=int) # to address the issue of classes being sorted as strings
         dataset.class_to_idx = {cls: i for i, cls in enumerate(dataset.classes)}
+        id2class = {dataset.class_to_idx[c] : py_vars.num2class_v2[int(c)] for c in dataset.classes}
     elif dataset_name == "imagenet_a":
         dataset = datasets.ImageFolder(root="./data/imagenet-a", transform=transform)
+        dataset.class_to_idx = {cls: i for i, cls in enumerate(dataset.classes)}
+        id2class = {dataset.class_to_idx[c] : py_vars.num2class[c] for c in dataset.classes}
     else:
         raise ValueError(f"Unknown dataset {dataset_name}")
     
@@ -34,10 +39,15 @@ def get_data(dataset_name, batch_size, transform, shuffle=True, train_size=0.8, 
     n_train = int(train_size * n)
     n_val = int(val_size * n)
     n_test = n - n_train - n_val
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [n_train, n_val, n_test])
+    
+    if(n_train + n_val == 0):
+        train_loader, val_loader = None, None
+        test_loader = data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    else:
+        train_dataset, val_dataset, test_dataset = random_split(dataset, [n_train, n_val, n_test])
 
-    train_loader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
-    val_loader = data.DataLoader(val_dataset, batch_size=batch_size, shuffle=shuffle)
-    test_loader = data.DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
+        train_loader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+        val_loader = data.DataLoader(val_dataset, batch_size=batch_size, shuffle=shuffle)
+        test_loader = data.DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
 
-    return train_loader, val_loader, test_loader, dataset.classes, dataset.class_to_idx
+    return train_loader, val_loader, test_loader, list(id2class.values()), id2class
