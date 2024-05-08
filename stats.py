a7307f1694f690694ab2819b7b8078af85b9f4c9
Amir <amir.gheser@studenti.unitn.it>
2024-05-08 16:45:54 +0200

    Stats by Carlo
    Co-authored-by: MasterCarlo <MasterCarlo@users.noreply.github.com>

--- stats.py
+++ stats.py
@@ -6,28 +6,37 @@
 
 # !TODO: Implement the following functions
 # !TODO: Find other ways to neatly represent results
+# !TODO: Find graph to describe/measure the confidence of the predictions/classes
 # Common statistics used to evaluate models with classification?
 
 from typing import Optional
 from sklearn.metrics import confusion_matrix as confusion_matrix_comp
 import matplotlib.pyplot as plt
 import numpy as np
-import itertools
-import io
 import os
+from torch.utils.tensorboard import SummaryWriter
+from utils import get_index
+
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import tensorflow as tf
+LOG_DIR = "tmp/"
 
-LOG_DIR = "tmp/conf_mat"
-
-def confusion_matrix(true_labels: list[int], predicted_labels: list[int], class_names: list[str], save_path:str=Optional[str]):
+def confusion_matrix(true_labels: list[int], predicted_labels: list[int], class_names: list[str], save_path:str=Optional[str])->tuple:
+    """
+    Computes and returns the confusion matrix given a batch of images and labels
+    Then plots the results
+    :param true_labels: list: ground truth labels
+    :param predicted_labels: list: predicted labels
+    :param class_names: list: class names
+    :return: tuple: confusion matrix and figure
+    """
     assert len(true_labels) == len(predicted_labels), "Differing prediction and ground truth size" 
 
     cm = confusion_matrix_comp(true_labels, predicted_labels)
-    # Plot confusion matrix
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-    plt.title('Confusion matrix')
-    plt.colorbar()
+
+    fig, ax = plt.subplots()
+    cax = ax.matshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    fig.colorbar(cax)
+
     tick_marks = np.arange(len(class_names))
     plt.xticks(tick_marks, classes)
     plt.yticks(tick_marks, classes)
@@ -41,71 +50,30 @@ def confusion_matrix(true_labels: list[int], predicted_labels: list[int], class_
     plt.xlabel('Predicted label')
     plt.tight_layout()
 
-    plt.show()
-    return cm
+    return fig, cm
 
 
-# def confusion_matrix(cm: np.ndarray, class_names: str=[], batch_size:int = 10, save_path:str=Optional[str]):
-#     """
-#     Computes and returns/plots the confusion matrix given a batch of images and labels
-#     """
-    
-#     figure = plt.figure(figsize=(8, 8))
-#     plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-#     plt.title("Confusion matrix")
-#     plt.colorbar()
-#     tick_marks = np.arange(len(class_names))
-#     plt.xticks(tick_marks, class_names, rotation=45)
-#     plt.yticks(tick_marks, class_names)
-    
-#     # Normalize the confusion matrix.
-#     cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
-    
-#     # Use white text if squares are dark; otherwise black.
-#     threshold = cm.max() / 2.
-    
-#     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-#         color = "white" if cm[i, j] > threshold else "black"
-#         plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
-        
-#     plt.tight_layout()
-#     plt.ylabel('True label')
-#     plt.xlabel('Predicted label')
-    
-#     buf = io.BytesIO()
-    
-#     # Use plt.savefig to save the plot to a PNG in memory.
-#     plt.savefig(buf, format='png')
-    
-#     # Closing the figure prevents it from being displayed directly inside
-#     # the notebook.
-#     plt.close(figure)
-#     buf.seek(0)
-    
-#     # Use tf.image.decode_png to convert the PNG buffer
-#     # to a TF image. Make sure you use 4 channels.
-#     figure = tf.image.decode_png(buf.getvalue(), channels=4)
-    
-#     # Use tf.expand_dims to add the batch dimension
-#     figure = tf.expand_dims(figure, 0)
-    
-#     return figure
-
-
-def average_class_error(k:int = 1, save_path=Optional[str]):
+def average_class_error(cm, k:int = 1, save_path=Optional[str]):
     """
     Computes and returns the average class error given a batch of images and labels
     Then plots the results
     :param k: int: top k classes to consider
     :param save_path: str: path to save the results
     """
-    pass
+    # Compute class-wise error
+    class_error = 1 - np.diag(cm) / np.sum(cm, axis=1)
+    
+    class_average_error = np.mean(class_error)
+    return class_average_error
 
 if __name__ == "__main__":
+    writer = SummaryWriter(LOG_DIR)
     true_labels = [0, 1, 2, 0, 1, 2, 0, 2]
     predicted_labels = [0, 1, 1, 0, 1, 2, 0, 2]
     classes = ['Class 0', 'Class 1', 'Class 2']
-    cm=confusion_matrix(true_labels, predicted_labels, classes)
-    plt.imsave(LOG_DIR, cm)
-    
-    
+    fig, cm = confusion_matrix(true_labels, predicted_labels, classes)
+    writer.add_figure("Confusion Matrix", fig)
+    idx = get_index(f"{LOG_DIR}/conf_mat")
+    fig.savefig(f"{LOG_DIR}/conf_mat/confusion_matrix_{idx}.png")
+    class_average_error = average_class_error(cm)
+    print("Average Class Error:", class_average_error)
