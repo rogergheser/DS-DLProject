54d80c183dfd1b5e60bdeca611875c44afa613dd
MasterCarlo <carlomagnoccml@gmail.com>
2024-05-08 18:15:15 +0200

    plotter for average_class_error

--- stats.py
+++ stats.py
@@ -14,10 +14,10 @@ from sklearn.metrics import confusion_matrix as confusion_matrix_comp
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-#from utils import get_index
+from utils import get_index
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from torch.utils.tensorboard import SummaryWriter
-LOG_DIR = "tmp/"
+LOG_DIR = "tmp"
 
 def confusion_matrix(true_labels: list[int], predicted_labels: list[int], class_names: list[str], save_path:str=Optional[str])->tuple:
     """
@@ -52,15 +52,35 @@ def confusion_matrix(true_labels: list[int], predicted_labels: list[int], class_
     return fig, cm
 
 
-def average_class_error(cm, k:int = 1, save_path=Optional[str]):
+def average_class_error(cm, class_names: list[str], k:int = 1, save_path: Optional[str]=None):
     """
     Computes and returns the average class error given a batch of images and labels
     Then plots the results
+    :param cm: np.array: confusion matrix
+    :param class_names: list: class names
     :param k: int: top k classes to consider
     :param save_path: str: path to save the results
     """
     # Compute class-wise error
     class_wise_error = 1 - np.diag(cm) / np.sum(cm, axis=1)
+    
+    # Sort class error and class names based on error rates
+    sorted_indices = np.argsort(class_wise_error)  # Sort in descending order
+    sorted_class_error = class_wise_error[sorted_indices]
+    sorted_class_names = [class_names[i] for i in sorted_indices]
+    # Plot
+    plt.figure(figsize=(10, 6))
+    plt.barh(range(len(sorted_class_names)), sorted_class_error, color='blue')
+    plt.ylabel('Classes')
+    plt.xlabel('Error Rate')
+    plt.title('Error Rate per Class')
+    plt.yticks(range(len(sorted_class_names)), sorted_class_names)
+    plt.tight_layout()
+    
+    if save_path:
+        plt.savefig(save_path)
+    else:
+        plt.show()
     return class_wise_error
 
 if __name__ == "__main__":
@@ -72,5 +92,5 @@ if __name__ == "__main__":
     writer.add_figure("Confusion Matrix", fig)
     idx = get_index(f"{LOG_DIR}/conf_mat")
     fig.savefig(f"{LOG_DIR}/conf_mat/confusion_matrix_{idx}.png")
-    class_wise_error = average_class_error(cm)
+    class_wise_error = average_class_error(cm, classes)
     print("Average Class Error:", class_wise_error)
