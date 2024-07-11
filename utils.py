9002bc6c6e3d4860c6fa8f6d9b6d894c4787e5e0
MasterCarlo <carlomagnoccml@gmail.com>
2024-07-11 16:27:01 +0200

    notebook create dand functions copy pasted on utils

--- utils.py
+++ utils.py
@@ -84,3 +84,110 @@ def avg_entropy(outputs):
     min_real = torch.finfo(avg_logits.dtype).min
     avg_logits = torch.clamp(avg_logits, min=min_real)
     return -(avg_logits * torch.exp(avg_logits)).sum(dim=-1)
+
+def load_pretrained_coop(backbone, _model):
+    if backbone.lower() == "rn50":
+        _backbone = "rn50"
+    elif backbone.lower() == "rn101":
+        _backbone = "rn101"
+    elif backbone.lower() == "vit_b16" or backbone.lower() == "vit-b/16":
+        _backbone = "vit_b16"
+    elif backbone.lower() == "vit_b32" or backbone.lower() == "vit-b/32":
+        _backbone = "vit_b32"
+    else:
+        raise ValueError(f"Unknown backbone {backbone}")
+
+    path = f"bin/coop/{_backbone}_ep50_16shots/nctx4_cscFalse_ctpend/seed1/prompt_learner/model.pth.tar-50"
+    assert os.path.exists(path), f"Path {path} does not exist"
+
+    pretrained_ctx = torch.load(path, DEVICE)['state_dict']['ctx']
+    assert pretrained_ctx.size()[0] == _model.prompt_learner.n_ctx, f"Number of context tokens mismatch: {_model.prompt_learner.n_ctx} vs {pretrained_ctx.size()[0]}"
+    with torch.no_grad():
+        _model.prompt_learner.ctx.copy_(pretrained_ctx)
+        _model.prompt_learner.ctx_init_state = pretrained_ctx
+
+def batch_report(inputs, outputs, final_prediction, targets, id2classes, batch_n):
+    from matplotlib import pyplot as plt
+    probabilities, predictions = outputs.cpu().topk(5)
+    probabilities = probabilities.detach().numpy()
+    predictions = predictions.detach()
+
+    clip_mean = [0.48145466, 0.4578275, 0.40821073]
+    clip_std = [0.26862954, 0.26130258, 0.27577711]
+
+    mean = torch.tensor(clip_mean).reshape(1, 3, 1, 1)
+    std = torch.tensor(clip_std).reshape(1, 3, 1, 1)
+
+    # Denormalize the batch of images
+    # denormalized_images = inputs.cpu() * std + mean
+    # denormalized_images = denormalized_images.numpy().astype('uint8')
+    unnormalize = transforms.Normalize((-mean / std).tolist(), (1.0 / std).tolist())
+    denormalized_images = unnormalize(inputs)
+
+    # Visualise the input using matplotlib
+    images = [image.numpy().transpose(1, 2, 0) for image in denormalized_images.cpu()] # Convert to numpy and transpose to (H, W, C)
+
+    # Visualise the input using matplotlib
+    label = id2classes[targets[0].item()]
+    plt.figure(figsize=(16,16))
+    plt.title(f"Image batch of {label} - min entropy 10 samples selected")
+    plt.axis('off')
+
+    for i, image in enumerate(images[:10]):
+        plt.subplot(6,4, 2*i+1)
+        plt.imshow(image)
+        plt.axis('off')
+
+        plt.subplot(6,4, 2*i+2)
+        y = np.arange(probabilities.shape[-1])
+        plt.grid()
+        plt.barh(y, probabilities[i])
+        plt.gca().invert_yaxis()
+        plt.gca().set_axisbelow(True)
+        plt.yticks(y, [id2classes[pred] for pred in predictions[i].numpy()])
+        plt.xlabel("probability")
+    
+    avg_prob, avg_pred = final_prediction.cpu().topk(5)
+    avg_prob = avg_prob.detach().numpy()
+    avg_pred = avg_pred.detach()
+    plt.subplot(6,4,22)
+    y = np.arange(avg_prob.shape[-1])
+    plt.grid()
+    plt.barh(y, avg_prob[0])
+    plt.gca().invert_yaxis()
+    plt.gca().set_axisbelow(True)
+    plt.yticks(y, [id2classes[index] for index in avg_pred[0].numpy()])
+    plt.xlabel("Final prediction (avg entropy)")    
+
+    plt.savefig(f"batch_reports/Batch{batch_n}.png")
+    plt.close()
+
+def make_histogram(no_tpt_acc: dict, tpt_acc: dict, no_tpt_label: str, tpt_label: str, save_path:str=None)-> Image:
+    """
+    Creates histogram for class accuracies and log it with tensorboard and saves the plot
+    """
+    classes = list(no_tpt_acc.keys())
+    x = np.arange(len(classes))
+    width = 0.35
+
+    fig, ax = plt.subplots(dpi=500)
+    ax.bar(x - width/2, no_tpt_acc.values(), width, color='b', label=no_tpt_label)
+    ax.bar(x + width/2, tpt_acc.values(), width, color='r', label=tpt_label)
+    
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Class accuracies')
+    ax.set_xticks(x)
+    ax.set_xticklabels(classes, rotation=-90, fontsize=2)
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    image = Image.open(buf)
+    image = np.array(image)
+
+    if save_path:
+        plt.savefig(save_path)
+
+    return image
+
