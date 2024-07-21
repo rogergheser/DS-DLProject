exp5 - 57 top1 - 1042 it -fixed plotting bugs -changed augmix operations -introduced 2 ways to average predictions

tpt_eval.py
@@ -29,10 +29,12 @@ from copy import deepcopy
 import torch.nn.functional as F
 import logging
 
-DEBUG = True
+DEBUG = False
+HARMONIC_MEAN=True
 RUN_NAME = "exp5"
-LOG_FREQUENCY = 10
+LOG_FREQUENCY = 40
 logger = logging.getLogger(__name__)
+
 
 def add_caption_loss(net: OurCLIP, captioner: Captioner, batch, text_features, id2classes, prompt="a ", _lambda=0, K=200, debug=False):
     """
@@ -61,8 +63,11 @@ def add_caption_loss(net: OurCLIP, captioner: Captioner, batch, text_features, i
     
     # Encode all the captions using the clip encoder (batchfying the captions to save compute)
     caption_tokens = clip.tokenize(captions).to(device)
-    caption_features = net.encode_text(caption_tokens).to(device) 
+    caption_features = net.encode_text(caption_tokens).to(device)
+
+    
     caption_logits = net.logit_scale.exp()*(F.normalize(caption_features) @ text_features.T)
+
     caption_logits = caption_logits.softmax(-1)
     image_logits = filtered_outputs
 
@@ -83,10 +88,12 @@ def add_caption_loss(net: OurCLIP, captioner: Captioner, batch, text_features, i
             A = 1/(1 + entropy(image_logits[batch]).item())
             B = 1/(1 + entropy(caption_logits[batch]).item())
             C = A + B
-            ice_scores[batch] = (A/C * image_logits[batch] + B/C * caption_logits[batch]).softmax(-1)
-            ice_scores[batch] = (2 * image_logits[batch] * caption_logits[batch]).div(image_logits[batch] + caption_logits[batch])
-    caption_prediction = torch.mean(caption_logits, dim=0)
+            if HARMONIC_MEAN:
+                ice_scores[batch] = (2 * image_logits[batch] * caption_logits[batch]).div(image_logits[batch] + caption_logits[batch])
+            else:
+                ice_scores[batch] = (A/C * image_logits[batch] + B/C * caption_logits[batch])
 
+    caption_prediction = torch.mean(caption_logits, dim=0)
     if debug:
         caption_report(filtered_inputs, image_logits, caption_logits, ice_scores, label, captions, caption_prediction, id2classes, batch_idx)    
 
@@ -195,6 +202,8 @@ def tpt_train_loop(data_loader, net, optimizer, cost_function, scaler, writer, i
             # Log Values
             writer.add_scalar("Delta_loss/test", loss_diff, batch_idx)
             writer.add_scalar("Delta_entropy/test", entropy_diff, batch_idx)
+            writer.add_scalar("Top-1", top1/samples*100.00, batch_idx)
+            writer.add_scalar("Top-5", top5/samples*100.00, batch_idx)
             if batch_idx % LOG_FREQUENCY == 0:
                 logger.info(f"[LOSS] Batch {batch_idx} - Delta loss: {loss_diff:.5f}, Delta entropy: {entropy_diff:.5f}")
                 no_tpt_accuracies, accuracies = compute_accuracies(id2classes, no_tpt_class_acc, tpt_class_acc)
@@ -222,17 +231,22 @@ def main(
     batch_size=64,
     learning_rate=0.005,
     tta_steps=2,
-    run_name="exp6",
+    run_name=RUN_NAME,
     n_ctx=4,
     ctx_init="a_photo_of_a",
     class_token_position="end",
     csc=False,
     ice_loss=True,
+    harmonic_mean=HARMONIC_MEAN,
     debug=DEBUG
 ):
+    HARMONIC_MEAN = harmonic_mean
+    DEBUG = debug
+    RUN_NAME = run_name
 
-    print("Using manual seed")
-    torch.manual_seed(0)
+    seed = 0
+    print("Using manual seed {}".format(seed))
+    torch.manual_seed(seed)
     # Create a logger for the experiment
     run_name = RUN_NAME
     writer = SummaryWriter(log_dir=f"runs/{run_name}")
@@ -288,7 +302,7 @@ def main(
     test_loss, test_accuracy = tpt_train_loop(test_loader, net, optimizer, cost_function, scaler, writer, id2classes=id2class, device=device, captioner=captioner, debug=debug)
     print(f"\tTest loss {test_loss:.5f}, Test accuracy {test_accuracy:.2f}")
     
-    create_run_info(dataset_name, backbone, ice_loss, test_accuracy, run_name)
+    create_run_info(dataset_name, backbone, ice_loss, test_accuracy, run_name, harmonic_mean)
     
     writer.close()
 
@@ -304,7 +318,11 @@ if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
     os.makedirs(f"runs/{RUN_NAME}", exist_ok=True)
     
-    file_handler = logging.FileHandler(f"runs/{RUN_NAME}/log.log")
+    log_path = f"runs/{RUN_NAME}/log.log"
+    if os.path.isfile(log_path):
+        os.remove(log_path)
+
+    file_handler = logging.FileHandler(log_path)
     stderr_handler = logging.StreamHandler(sys.stderr)
 
     file_handler.setLevel(logging.DEBUG)

utils.py
@@ -364,18 +364,24 @@ def caption_report(images, image_logits, caption_logits, ice_scores, label, outp
     import matplotlib.pyplot as plt
 
     ice_probabilities, ice_predictions = ice_scores.topk(5)
-    cap_probabilities = torch.zeros(ice_predictions.shape)
-    for i in range(ice_predictions.shape[0]):
-        cap_probabilities[i,:] = caption_logits[i,ice_predictions[0]]
-
-    img_probabilities = torch.zeros(ice_predictions.shape)
-    for i in range(ice_predictions.shape[0]):
-        img_probabilities[i,:] = image_logits[i,ice_predictions[0]]
+    cap_probabilities = caption_logits.gather(1, ice_predictions)
+    img_probabilities = image_logits.gather(1, ice_predictions)
 
     ice_probabilities = ice_probabilities.cpu().detach()
     ice_predictions = ice_predictions.cpu().detach()
     cap_probabilities = cap_probabilities.cpu().detach()
-    img_probabilities = cap_probabilities.cpu().detach()
+    img_probabilities = img_probabilities.cpu().detach()
+    
+    # Debugging purposes
+    # with open(f"caption_reports/debug_{idx}.txt", 'w') as f:
+    #     f.write('\nice_predictions:\n')
+    #     np.savetxt(f, ice_predictions.numpy(), fmt='%d')
+    #     f.write('ice_probabilities:\n')
+    #     np.savetxt(f, ice_probabilities.numpy(), fmt='%f')
+    #     f.write('\ncap_probabilities:\n')
+    #     np.savetxt(f, cap_probabilities.numpy(), fmt='%f')
+    #     f.write('\nimg_probabilities:\n')
+    #     np.savetxt(f, img_probabilities.numpy(), fmt='%f')
 
     clip_mean = [0.48145466, 0.4578275, 0.40821073]
     clip_std = [0.26862954, 0.26130258, 0.27577711]
@@ -406,15 +412,16 @@ def caption_report(images, image_logits, caption_logits, ice_scores, label, outp
         width=0.35
         y = np.arange(ice_probabilities.shape[-1])
         plt.grid()
-        plt.barh(y-1/2*width, ice_probabilities[i], width, color="green")
-        plt.barh(y, cap_probabilities[i], width, color="red")
-        plt.barh(y+1/2*width, img_probabilities[i], width, color="blue")
+        plt.barh(y-width, ice_probabilities[i], width*2/3, color="green", label='ICE')
+        plt.barh(y, cap_probabilities[i], width*2/3, color="red", label='CAP')
+        plt.barh(y+width, img_probabilities[i], width*2/3, color="blue", label='IMG')
         plt.gca().invert_yaxis()
         plt.gca().set_axisbelow(True)
         plt.yticks(y, [id2class[pred] for pred in ice_predictions[i].numpy()])
         plt.xlim(0,1)
         plt.xlabel("probability")
-
+    
+    plt.legend()
     plt.subplots_adjust(hspace=0.5)  # Increase vertical space between subplots
 
     plt.savefig(f"caption_reports/batch_{idx}.png")
@@ -426,7 +433,8 @@ def create_run_info(dataset_name, backbone, ice_loss, test_accuracy, run_name):
         "backbone": backbone,
         "ice_loss": ice_loss,
         "top1": test_accuracy,
-        "exp_name": run_name
+        "exp_name": run_name,
+        "ice average": 'harmonic mean' if HARMONIC_MEAN else 'weighted average'
     }
     with open(f"runs/{run_name}/final_result.txt", "w") as file:
         json.dump(info, file)